149230684b64f7a4158a9432d9ce2d8c9109cdb2
Kartik Gupta <156841129+orignlkartik1@users.noreply.github.com>
2025-08-17 23:13:04 +0530

    Update bot.py

--- bot.py
+++ bot.py
@@ -1,7 +1,9 @@
 import telepot
 token = 'Api_Key_from_botfather'
 TelegramBot = telepot.Bot(token)
-print(TelegramBot.getMe())
+print(TelegramBot.getMe()) #it gives you metadata
 
-print(TelegramBot.getUpdates())
+print(TelegramBot.getUpdates()) #it gives you updates of your bot
+
+
 
