1e45d377402ed1d3a04995990761ce9fd8128c58
Kartik Gupta <156841129+orignlkartik1@users.noreply.github.com>
2025-08-17 23:15:18 +0530

    Update bot.py

--- bot.py
+++ bot.py
@@ -5,5 +5,7 @@ print(TelegramBot.getMe()) #it gives you metadata
 
 print(TelegramBot.getUpdates()) #it gives you updates of your bot
 
+# In console you got update ID you can use it for your new updates. 
+
 
 
